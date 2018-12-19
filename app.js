ad082afad5768afd63e204ad1a0842d01df5ec9f
Keerthana-Arumugam <45649939+Keerthana-Arumugam@users.noreply.github.com>
2018-12-19 12:37:10 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello Dev!');
+});
+app.listen(8080, function () {
+    console.log('Dev app listening on port 8080!');
+});
+    
+
