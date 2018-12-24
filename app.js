0af72705cb4295ec8c85bf16d42b373f130df642
Keerthana Arumugam <43020@corp.hexaware.com>
2018-12-24 11:36:39 +0530

    klj

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// const express = require('express')
-// const app = express()
-// const port = 8080
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
 var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
 
 
-app.get('/', function (req, res) {
-    res.send('Hello Dev!');
-});
+
 app.post('/',function(req,res){
     console.log("post method");
 
@@ -42,6 +31,6 @@ if(req.body.queryResult.intent.displayName == "sample")
 }
 });
 
-app.listen(5000, function () {
-    console.log('Dev app listening on port 5000!');
+app.listen(8080, function () {
+    console.log('Dev app listening on port 8080!');
 });
