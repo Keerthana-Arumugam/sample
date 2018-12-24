8aecfad4be90512e16f855d6c2f1469f97b5eb12
Keerthana Arumugam <43020@corp.hexaware.com>
2018-12-24 15:39:35 +0530

    using bodyparser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
 
+app.use(bodyParser.json());
 
 app.post('/',function(req,res){
     console.log("post method");
